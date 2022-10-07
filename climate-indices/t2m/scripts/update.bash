3d4ed695d233c163034e56072ad19a81c4e6df68
François Massonnet <francois.massonnet@uclouvain.be>
2022-10-07 19:25:06 +0200

    fix html issue

--- climate-indices/t2m/scripts/update.bash
+++ climate-indices/t2m/scripts/update.bash
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-python3 analysis_T2m.py
+set -o nounset
+set -o errexit
+set -x
+
+module purge
+module load releases/2020b
+module load ELIC_Python/1-foss-2020b
+#python3 analysis_T2m.py
 
 
-git add ../figs/T2m_Bruxelles_*.png
 
 # Create page with all years
 thisYear=`date +"%Y"`
 
-echo "<p align=\"center\">" > ../allYears.md
+outFile="../T2mAllYears.md"
 
-for year in `seq $(( $thisYear - 1)) 1959`
+echo "<p align=\"center\">" > $outFile
+for year in `seq $(( $thisYear - 1)) -1 1959`
 do
 	echo $year
-	echo "<h1> $year </h1>" >> ../allYears.md
-	echo "<img src=\"./figs/T2m_Bruxelles_$year.png\" width=\"1200\">" >> ../allYears.md
-	echo "<br>" >> ../allYears.md
+	echo "<h1> $year </h1>" >> $outFile
+	echo "<img src=\"./figs/T2m_Bruxelles_$year.png\" width=\"1200\">" >> $outFile
+	echo "<br>" >> $outFile
 done
 
-echo "</p>" >> ../allYears.md
+echo "</p>" >> $outFile
 
+git add ../figs/T2m_Bruxelles_*.png
 
 git commit -m "Daily update"
 
